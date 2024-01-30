5c2d0fc9bf8c5e9e219c00fda33c294393ec3583
hadarotmi <hadarotmi123@gmail.com>
2024-01-30 11:57:25 +0200

    Add existing project files to Git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { createTheme, ThemeProvider, useTheme } from "@mui/material/styles";
+import rtlPlugin from "stylis-plugin-rtl";
+import { prefixer } from "stylis";
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
+import { Provider } from "react-redux";
+// import { configureStore } from "@reduxjs/toolkit";
+// import { useMediaQuery } from "@mui/material";
+import HomePage from './Components/homePage';
 
-function App() {
+const theme = createTheme({
+  direction: "rtl",
+  palette: {
+    primary: {
+      main: "#077777",
+      light: "#C0DEDE",
+    },
+    secondary: {
+      main: "#f2f2f4",
+      dark: "#c7c7ca",
+    },
+  },
+  typography: {
+    fontFamily: `'Rubik', sans-serif`,
+  },
+});
+
+const cacheRtl = createCache({
+  key: "muirtl",
+  stylisPlugins: [prefixer, rtlPlugin],
+});
+
+// const myStore = configureStore({
+//   reducer: {
+//     sadamReducer,
+//     userReducer,
+//     reportReducer,
+//   },
+// });
+
+const App=() => {
+  // const isMobile = useMediaQuery(useTheme().breakpoints.down("sm"));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CacheProvider value={cacheRtl}>
+      <ThemeProvider theme={theme}>
+        {/* <Provider store={myStore}> */}
+          <HomePage/>
+        {/* </Provider> */}
+      </ThemeProvider>
+    </CacheProvider>
   );
-}
+};
 
 export default App;
